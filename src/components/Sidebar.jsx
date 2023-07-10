d9b019bd850357138557fcb379eb9b3effad67be
Pedro <pedrovilarinho.gpd@gmail.com>
2023-07-10 11:03:36 -0300

    Feat sidebar and phosphor icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,30 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
-    
-     <div className={styles.profile}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/pedroeduardo36.png"
+        />
+
         <strong>Pedro Andrade</strong>
         <span>Dev Front-end</span>
-     </div>
-     <footer>
-        <a href="#">Editar seu perfil</a>
-     </footer>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
     </aside>
   );
 }
