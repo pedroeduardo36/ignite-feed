26fed495f77fb847172fd64bb53f3a9be461c33c
Pedro <pedrovilarinho.gpd@gmail.com>
2023-07-10 11:58:46 -0300

    component: Post

--- src/Post.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-export function Post(props) {
-    return (
-        <div>
-            <strong>{props.author}</strong>
-            <p>{props.content}</p>
-        </div>
-    )
-}
---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/pedroeduardo36.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Pedro Andrade</strong>
+            <span>Front end developer</span>
+          </div>
+        </div>
+
+        <time
+          title="publicado 10 de julho às 10:53"
+          dateTime="2022-07-10  11:53:00"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p> Fala galera</p>
+
+        <p>Passando pra dizer que </p>
+
+        <p>
+          Sou o homem mais sortudo do mundo, por conhecer a garota mais incrível
+          de todas
+        </p>
+
+        <p><a href="">Texto só pra por um link</a></p>
+      </div>
+    </article>
+  );
+}
