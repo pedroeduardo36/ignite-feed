386f73d08149970d04063ee7302abd6883cfc549
Pedro <pedrovilarinho.gpd@gmail.com>
2023-07-10 16:06:57 -0300

    Formulário de comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,8 +33,20 @@ export function Post() {
           de todas
         </p>
 
-        <p><a href="">Texto só pra por um link</a></p>
+        <p>
+          <a href="">Texto só pra por um link</a>
+        </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
