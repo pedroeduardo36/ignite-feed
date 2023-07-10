431f009e05ea91ba45e859b59caa61a3da63725d
Pedro <pedrovilarinho.gpd@gmail.com>
2023-07-10 10:09:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Pedro Andrade"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae hic velit minima dignissimos ducimus iure a! Asperiores, error! Nam, saepe eaque reiciendis corporis distinctio nulla similique aliquam ipsam alias molestias."
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
