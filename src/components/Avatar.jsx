6eb11088ece5fdfdee81965bc771ce0039d56184
Pedro <pedrovilarinho.gpd@gmail.com>
2023-07-10 17:59:00 -0300

    Componente: Avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,14 @@
+import styles from "./Avatar.module.css";
+
+// eslint-disable-next-line react/prop-types
+export function Avatar({hasBorder = true, src}) {
+  // eslint-disable-next-line react/prop-types
+  return (
+    <img
+      // eslint-disable-next-line react/prop-types
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      // eslint-disable-next-line react/prop-types
+      src={src}
+    />
+  );
+}
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="http://github.com/pedroeduardo36.png" alt="" />
+      <Avatar hasBorder={false} src="http://github.com/pedroeduardo36.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
